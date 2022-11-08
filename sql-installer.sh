2f2d3a1d4a99b5f323d25326773371c294680ee4
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-11-07 21:21:54 -0500

    Update sql-installer.sh

--- sql-installer.sh
+++ sql-installer.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# An installer for mySQL seeing how the installer that comes with it is broken on ubuntu
-
 if [ $EUID -ne 0 ]
 	then
 		echo "This program must run as root to function." 
@@ -11,11 +9,12 @@ fi
 # to see if the service is already started
 
 systemctl start mysql.service &&
-
 echo "enter in ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Password'; then enter/type in exit;"
 mysql &&
-#echo "resetting the mysql service..."
-#systemctl restart mysql.service --now &&
+echo "Keep in mind that you will need to enter this password I had just given you "
+echo "for root, I would advise that you'd change it."
+echo "resetting the mysql service..."
+systemctl restart mysql.service --now &&
 # mysql -u root -p &&
 
 echo "ok you have now set up the service, now you'll need to "
