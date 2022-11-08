422aeb9b8048ee5620b402d0bb0c304275882dee
Nicholas <68661602+Nthompson096@users.noreply.github.com>
2022-11-07 20:56:47 -0500

    Create sql-installer.sh
    An installer for mySQL seeing how the installer that comes with it is broken on ubuntu

---
+++ sql-installer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# An installer for mySQL seeing how the installer that comes with it is broken on ubuntu
+
+if [ $EUID -ne 0 ]
+	then
+		echo "This program must run as root to function." 
+		exit 1
+fi
+
+# to see if the service is already started
+
+systemctl start mysql.service &&
+
+echo "enter in ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Password'; then enter/type in exit;"
+mysql &&
+#echo "resetting the mysql service..."
+#systemctl restart mysql.service --now &&
+# mysql -u root -p &&
+
+echo "ok you have now set up the service, now you'll need to "
+# echo "mysql -u<ser> root -p<assword> enter the command"
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH auth_socket; then enter/type in exit"
+echo "continue the install with"
+echo "sudo mysql_secure_installation"
